efe18cf733671566f8b179a4152c53a120a344b0
Johannes Maurer <johannes.maurer@ist.tugraz.at>
2014-06-04 11:21:54 +0200

    schunk_lwa4p_control added

---
+++ schunk_lwa4p_control/include/schunk_lwa4p_control/schunk_lwa4p_control.hpp
@@ -0,0 +1,69 @@
+/*********************************************************************
+ *
+ *  Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014, Johannes Maurer, Alexander Buchegger
+ *                      Institute for Software Technology,
+ *                      Graz University of Technology
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Graz University of Technology nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *********************************************************************/
+
+#ifndef schunk_lwa4p_control_hpp___
+#define schunk_lwa4p_control_hpp___
+
+#include <ros/ros.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+
+namespace schunk_lwa4p_control
+{
+
+class SchunkLWA4P : public hardware_interface::RobotHW
+{
+public:
+    SchunkLWA4P();
+
+private:
+    ros::NodeHandle nh_;
+
+    hardware_interface::JointStateInterface joint_state_interface;
+    hardware_interface::PositionJointInterface joint_pos_interface;
+
+    double cmd[2];
+    double pos[2];
+    double vel[2];
+    double eff[2];
+};
+
+}
+
+#endif // schunk_lwa4p_control_hpp___
