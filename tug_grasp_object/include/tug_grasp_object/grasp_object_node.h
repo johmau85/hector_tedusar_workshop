08c47c189526885708556a246087527db96e58c0
Julia Nitsch <julia.nitsch@student.tugraz.at>
2014-06-06 20:09:50 +0200

    basic stuff in aciton added

---
+++ tug_grasp_object/include/tug_grasp_object/grasp_object_node.h
@@ -0,0 +1,26 @@
+#ifndef GRASP_OBJECT_NODE
+#define GRASP_OBJECT_NODE
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/server/simple_action_server.h>
+#include
+
+
+namespace grasp_object
+{
+class GraspObjectNode
+{
+public:
+    GraspObjectNode();
+
+    virtual ~GraspObjectNode();
+private:
+
+
+
+};
+}
+
+
+
+#endif
---
+++ tug_grasp_object/src/grasp_object_node.cpp
@@ -0,0 +1,20 @@
+#include<tug_grasp_object/grasp_object_node.h>
+
+int main(int argc, char** argv)
+{
+//    try
+//    {
+//        ros::init(argc,argv,"navigation_node");
+
+//        tedusar_navigation::NavigationNode navigation_node;
+
+//        ros::spin();
+//    }
+//    catch(...)
+//    {
+//        ROS_ERROR("Unhandled exception!");
+//        return -1;
+//    }
+
+    return 0;
+}
